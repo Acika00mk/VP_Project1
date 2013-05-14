5dbaea1a60c6b1c1bfb89b4ed103f6954c2c9f6b
Aleksandar Kostadinovski <aleksandar@kostadinovski.info>
2013-05-14 13:34:46 -0700

    Start up

--- VP/VP_Project1/Form1.Designer.cs
+++ VP/VP_Project1/Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- VP/VP_Project1/Form1.cs
+++ VP/VP_Project1/Form1.cs
@@ -16,5 +16,46 @@ namespace VP_Project1
         {
             InitializeComponent();
         }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyValue.ToString())
+            {
+                case "39" :
+                    label1.Text = "39";
+                label1.Left += 5;
+                    break;
+                case "40":
+                    label1.Text = "40";
+                    label1.Top += 5;
+                    break;
+                case "37":
+                    label1.Text = "37";
+                    label1.Left -= 5;
+                    break;
+                case "38":
+                    label1.Text = "39";
+                    label1.Top -= 5;
+                    break;
+                case "13":
+                    MessageBox.Show("Fire Missle");
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Pen a = new Pen(Color.Red,4);
+            Graphics g = this.CreateGraphics();
+            g.DrawEllipse(a, e.X-60, e.Y-60, 120, 120);
+            
+        }
     }
 }
